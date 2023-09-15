bc7c9817406a86ffccd08d898f098fc4c023da2e
Kwonyoungoh <rnjsduddh05@gmail.com>
2023-09-15 16:39:48 +0900

    if

--- 0915/py12_id01.py
+++ 0915/py12_id01.py
@@ -9,3 +9,19 @@ print(c, ':', id(c))
 print(d, ':', id(d))
 
 print('같은 객체' if c is d else '다른 객체')
+
+# 그러나 배열에서는 다른객체다 왤까?
+# 리스트같은 경우는 가변(mutable)이기 때문에 다른주소 같은 값을 바라보더라도
+lst1 = [1, 2, 3]
+lst2 = [1, 2, 3]
+print('같은 객체' if lst1 is lst2 else '다른 객체')
+print(id(lst1))
+print(id(lst2))
+
+# 그러나 튜플에서는 같은객체다
+# 결국 immutable이기때문에 같은 주소를 가진다.
+lst1 = (1, 2, 3)
+lst2 = (1, 2, 3)
+print('같은 객체' if lst1 is lst2 else '다른 객체')
+print(id(lst1))
+print(id(lst2))
---
+++ 0915/py13_if01.py
@@ -0,0 +1,21 @@
+a1 = 3
+b = 2
+
+print('참' if a1 == 3 else '거짓')
+
+if a1 == 3:
+    print('charm')
+else:
+    print('ah nim')
+
+if a1 is b :
+    pass
+else:
+    print('dd')
+
+# b1과 b2의 값을 입력받아서 합이 10보다 크면 '출력'
+
+a1 = int(input('a1 number : '))
+b1 = int(input('b1 number : '))
+
+print('이얏호' if (a1+b1)>0 else '')
