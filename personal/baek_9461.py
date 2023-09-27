96b56756df8a1511d803a9db1d7f783b8f7b259e
Kwonyoungoh <rnjsduddh05@gmail.com>
2023-09-27 18:38:26 +0900

    9461풀이

--- personal/baek_9461.py
+++ personal/baek_9461.py
@@ -1,1 +1,17 @@
+from sys import stdin
 # 파도반 수열
+# T
+T = int(stdin.readline().rstrip())
+ns = [int(stdin.readline().rstrip()) for _ in range(T)]
+lst = [0,1,1,1,2,2,3,4,5,7,9]
+
+for n in ns:
+
+    if n < len(lst):
+        print(lst[n])
+        continue
+
+    for i in range(len(lst)-1,n+1):
+        lst.append(lst[i]+lst[i-4])
+
+    print(lst[n])
