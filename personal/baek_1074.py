6a8d6cdf0576a32f7df2fe93a9aa3805d140e239
Kwonyoungoh <rnjsduddh05@gmail.com>
2023-10-26 01:40:52 +0900

    1074 시간초과로 재풀이

--- personal/baek_1074.py
+++ personal/baek_1074.py
@@ -1,21 +1,45 @@
 from sys import stdin
 # 1074
 
-# DFS 문제
+# DFS 인줄
+# 재귀
+def self_z(n,x,y,val,c,r):
+
+    if n==1:
+        if x == c and y == r:
+            return val[0]
+        val[0] += 1
+    else:
+    # 계속 넷으로 나눈다
+        res = self_z(n//2,x,y,val,c,r)
+        if res is not None:
+            return res
+
+        res = self_z(n//2,x+n//2,y,val,c,r)
+        if res is not None:
+            return res
+
+        res = self_z(n//2,x,y+n//2,val,c,r)
+        if res is not None:
+            return res
+
+        res = self_z(n//2,x+n//2,y+n//2,val,c,r)
+        if res is not None:
+            return res
+
 
 # n r c
 nrc = list(map(int,stdin.readline().rstrip().split(' ')))
 
-n = nrc[0]
+N = nrc[0]
 r = nrc[1]
 c = nrc[2]
 
-lst = [0]*(2**n)
-ans = []
-for i in range(2**n):
-    ans.append(lst)
-print(ans)
+# lst = [0]*(2**N)
+# ans = [[0] * (2**N) for _ in range(2**N)]
+val1 = [0]
 # 방문순서
 # 걍 N*N을 방문순서대로 1부터 넣자
 
-for i in range((2**n)**(2**n)):
+
+print(self_z(2**N,0,0,val1,c,r))
